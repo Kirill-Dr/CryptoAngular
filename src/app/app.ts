845e4ab1ee10073251efc2a298a8f9416acc69e3
Kirill-Dr <daright6@gmail.com>
2026-05-11 10:34:12 +0600

    new component button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Button } from './shared/components/button/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Button],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('CryptoAngular');
